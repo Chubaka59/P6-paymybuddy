adc09656c4a60bb65d4e1c09608239d5ba819eeb
Joffrey <joffrey.lefebvre@gmail.com>
2023-04-22 20:41:16 +0200

    added validation in ReloadDto

--- src/main/java/com/openclassrooms/paymybuddy/dto/ReloadDto.java
+++ src/main/java/com/openclassrooms/paymybuddy/dto/ReloadDto.java
@@ -1,11 +1,13 @@
 package com.openclassrooms.paymybuddy.dto;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class ReloadDto {
+    @Digits(integer = 3, fraction = 2)
     BigDecimal amount;
 }
